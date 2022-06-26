59600c3f5e3aeca4005a8c3060f7bd2b62070ab7
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-06-27 00:45:46 +0300

    modal-products

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,36 @@
 (() => {
   const refs = {
-    openProductBtn: document.querySelector('[data-product-open]'),
-    closeProductBtn: document.querySelector('[data-product-close]'),
-    menu: document.querySelector('[data-product]'),
+    openProductBtn: document.querySelector('[data-iceCream-open]'),
+    closeProductBtn: document.querySelector('[data-iceCream-close]'),
+    menu: document.querySelector('[data-iceCream]'),
+  };
+
+  refs.openProductBtn.addEventListener('click', toggleProduct);
+  refs.closeProductBtn.addEventListener('click', toggleProduct);
+
+  function toggleProduct() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openProductBtn: document.querySelector('[data-iceCoffee-open]'),
+    closeProductBtn: document.querySelector('[data-iceCoffee-close]'),
+    menu: document.querySelector('[data-iceCoffee]'),
+  };
+
+  refs.openProductBtn.addEventListener('click', toggleProduct);
+  refs.closeProductBtn.addEventListener('click', toggleProduct);
+
+  function toggleProduct() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openProductBtn: document.querySelector('[data-milkshakes-open]'),
+    closeProductBtn: document.querySelector('[data-milkshakes-close]'),
+    menu: document.querySelector('[data-milkshakes]'),
   };
 
   refs.openProductBtn.addEventListener('click', toggleProduct);
